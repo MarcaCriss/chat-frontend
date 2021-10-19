28d5b45d6d4afc975970e8461e3851b416572b00
MarcaCriss <54193589+MarcaCriss@users.noreply.github.com>
2021-10-19 18:23:28 -0400

    pusher configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import Pusher from 'pusher-js';
 import { environment } from '../environments/environment';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+interface Message {
+  username: string,
+  message: string
+}
 
 @Component({
   selector: 'app-root',
@@ -9,17 +16,36 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent implements OnInit {
   title = 'chat-frontend';
+  username = 'username';
+  form!: FormGroup;
+  messages: Message[] = [];
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
     Pusher.logToConsole = true;
-
-    var pusher = new Pusher(environment.pusher_id!, {
+    const pusher = new Pusher(environment.pusher_id!, {
       cluster: environment.pusher_cluster
     });
-
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', (data: any) => {
-      alert(JSON.stringify(data));
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', (data: any) => {
+      this.messages.push(data);
     });
+    this.form = this.fb.group({
+      message: new FormControl('', [Validators.required])
+    })
+  }
+
+  submit() {
+    this.http.post('http://localhost:3000/messages',{
+      username: this.username,
+      message: this.form.value.message
+    }).subscribe(
+      () => {
+        this.form.setValue({
+          message: ''
+        })
+      }
+    )
   }
 }
